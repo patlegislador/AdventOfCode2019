309d5aae2bef734662bb37310241eab5679af87b
Patrick <52380430+patlegislador@users.noreply.github.com>
2019-12-09 18:06:25 -0500

    Incomplete day 3

--- Day 3/day3.js
+++ Day 3/day3.js
@@ -1,1 +1,25 @@
-//Day 3 - Crossed Wires
+const fs = require('fs')
+
+const path_one = {
+    X: 0,
+    Y: 0
+}
+
+const path_two = {
+    X: 0,
+    Y: 0
+}
+
+fs.readFile('Day 3/wires1.txt', (err, data) => {
+    if (err) throw err;
+
+    const wires1 = data.toString().split(',')
+
+    wires1.forEach(item => console.log(item))
+})
+
+fs.readFile('Day 3/wires2.txt', (err, data) => {
+    if (err) throw err;
+
+    const wires2 = data.toString().split(',')
+})
