3f197f9a52eaed56efa891b881a21f26e26711b0
Patrick <52380430+patlegislador@users.noreply.github.com>
2019-12-18 19:08:40 -0500

    Incomplete Day 3

--- Day 3/day3.js
+++ Day 3/day3.js
@@ -1,25 +1,34 @@
 const fs = require('fs')
 
-const path_one = {
-    X: 0,
-    Y: 0
-}
-
-const path_two = {
-    X: 0,
-    Y: 0
-}
-
 fs.readFile('Day 3/wires1.txt', (err, data) => {
     if (err) throw err;
 
+    const path_one = {
+        //determines current position
+        X: 0,
+        Y: 0
+    }
+
     const wires1 = data.toString().split(',')
 
-    wires1.forEach(item => console.log(item))
-})
+    wires1.forEach(item => {
+        let direction = item.charAt(0);
+        let steps = +(item.slice(1))
+        if (direction === 'R') path_one.X += steps
+        else if (direction === 'L') path_one.X -= steps
+        else if (direction === 'U') path_one.Y += steps
+        else if (direction === 'D') path_one.Y -= steps
+        else return
+    });
 
-fs.readFile('Day 3/wires2.txt', (err, data) => {
-    if (err) throw err;
 
-    const wires2 = data.toString().split(',')
+    fs.readFile('Day 3/wires2.txt', (err, data) => {
+        if (err) throw err;
+
+        const wires2 = data.toString().split(',')
+
+        wires2.forEach(item => {
+
+        })
+    })
 })
